4f80f11155824ea6423ee3180d0fb91b4db8a1c8
NaveedAfraz <naveedafraz123@gmail.com>
2025-06-29 16:53:54 +0530

    added ticket.jsx  and useTicket hook

---
+++ frontend/src/hooks/useTicket.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import axios from 'axios'
+function useTicket() {
+    const { id } = useParams();
+    const [ticket, setTicket] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const token = localStorage.getItem("token");
+
+    useEffect(() => {
+        const fetchTicket = async () => {
+            try {
+                const res = await axios.get(
+                    `${import.meta.env.VITE_SERVER_URL}/tickets/${id}`,
+                    { withCredentials: true }
+                )
+                console.log(res);
+                const data = await res.data;
+                console.log(data);
+                setTicket(data.ticket);
+            } catch (err) {
+                console.error(err);
+                setError("Something went wrong");
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchTicket();
+    }, [id]);
+
+    return {
+        ticket,
+        loading,
+        error
+    };
+}
+
+export default useTicket
